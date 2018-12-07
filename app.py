5e585fd362469d7e7aa30d51289fcd92992e98b0
James <james@lapthornconsulting.com>
2018-12-07 14:14:59 +0000

    changed default port to 8080

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 #!flask/bin/python
 from flask import Flask
 app = Flask(__name__)
-from flask import Flask
-from flask import request
-app = Flask(__name__)
-@app.route('/postjson', methods=['POST'])
-def post():
-    print(request.is_json)
-    content = request.get_json()
-    #print(content)
-    print(content['id'])
-    print(content['name'])
-    return 'JSON posted'
-app.run(host='0.0.0.0', port=5000)
+@app.route('/')
+def index():
+    return "Hello, World!"
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=8080)
