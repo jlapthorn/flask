5bc1480a6c211631fb863926452fefcd2b74bc26
James <james@lapthornconsulting.com>
2018-12-21 12:19:49 +0000

    Chnaged messagE

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "This is a test"
+    return "This is another test"
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=8080)
